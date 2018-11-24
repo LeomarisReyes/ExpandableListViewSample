daba57770ea3dcff1fc194d3bff729fb123b5547
Leomaris Reyes <reyes.leomaris@gmail.com>
2018-11-24 13:10:50 -0400

    .

---
+++ ExpandableListViewSample.iOS/Controls/CustomListViewRenderer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel;
+using ExpandableListViewSample.Controls;
+using ExpandableListViewSample.iOS.Controls;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CustomListView),typeof(CustomListViewRenderer))]
+namespace ExpandableListViewSample.iOS.Controls
+{
+    public class CustomListViewRenderer : ListViewRenderer
+    { 
+        public CustomListViewRenderer()
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<ListView> e)
+        {
+            base.OnElementChanged(e);
+            AnimationsEnabled = false;
+        }
+
+    }
+}
